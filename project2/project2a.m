6b10e82f156309c3dafc02c718eb73b2f02daecf
Kevin Joyce <kjoyce@callisto.localdomain>
2015-05-13 21:34:53 -0600

    All done except adding results slides

--- project2/project2a.m
+++ project2/project2a.m
@@ -21,20 +21,24 @@ q = phi*a'; % q vector from completing the square
 q_delta = q.restricted_to(-d,d); % Restrict (or zero pad) to the support [-d,d]. See FinSupFun.
 r = p \ q_delta; % Solving incorported in the FinSupFun class
 xhat0 = r.*y; % Construct estimate
+del = r*a; % Estimate of delta function
 
 % Calculate H
 H = phi.f(-phi.l+1) + q_delta.f*r.f'; % Theoretical H = f_0 + <q,P^(-1)q>
 
 figure(1)
+subplot(121)
 hb = plot(b.l:b.r, b.f,'g','LineWidth',3);
 hold on
 hf = plot(phi.l:phi.r, phi.f,'m','LineWidth',3);
 ha = plot(a.l:a.r, a.f,'b','LineWidth',3);
 hp = plot(p.l:p.r, p.f,'r','LineWidth',3);
+hr = plot(r.l:r.r, r.f,'c','LineWidth',3);
+hd = plot(del.l:del.r, del.f,'k','LineWidth',3);
 hold off
 grid on
-legend([hb hf ha hp], 'b', '\phi', 'a',  'p');
-title('Covariance PSFs for Deconvolution');
+legend([hb hf ha hp hr hd], {'b', '$\phi$', 'a',  'p', 'r', '$r * a$'}, 'Interpreter','Latex');
+title(sprintf('Covariance PSFs for Deconvolution d = %d',d));
 
 figure(2);
 subplot(311)
@@ -46,11 +50,11 @@ hold off
 grid on
 legend([hm hx hy], '\mu', 'x',  'y');
 
-subplot(312)
+subplot(313)
 hold all 
 plot(x.l:x.r,x.f,'LineWidth',3);
 plot(xhat0.l:xhat0.r,xhat0.f,'LineWidth',3) 
-%patch([xhat0.l:xhat0.r xhat0.r:-1:xhat0.l],[xhat0.f+sqrt(H) fliplr(xhat0.f-sqrt(H))], [0 1 0])%;, 'facealpha', .3);
+patch([xhat0.l:xhat0.r xhat0.r:-1:xhat0.l],[xhat0.f+sqrt(H) fliplr(xhat0.f-sqrt(H))], [0 1 0], 'facealpha', .3);
 hold off
 title(sprintf('r PSF radius d=%d',120))
 legend({'x','$\widehat{x}$, d=120'},'Interpreter','Latex')
@@ -72,7 +76,7 @@ for i = 1:length(d)
   H(i) = phi.f(-phi.l+1) - q_delta.f*r(i).f'; % Theoretical H = f_0 + <q,P^(-1)q>
 end 
 
-subplot(313)
+subplot(312)
 hold all 
 plot(x.l:x.r,x.f,'LineWidth',3);
 plot_idx = 1:ceil(length(xhat)/4):length(xhat); % Plot 4 reconstructions
@@ -84,7 +88,8 @@ title('Reconstructions for various d')
 legend(['x',arrayfun(@(d)( sprintf('$\\widehat{x}$, d=%d',d)),d(plot_idx),'UniformOutput',0)],'Interpreter','Latex')
 grid on
 
-figure(4)
+figure(1)
+subplot(122)
 title('PSFs for a*f')
 hold all
 for j = plot_idx
@@ -96,5 +101,5 @@ grid on
 legend([arrayfun(@(d)( sprintf('$r_d * a$, d=%d',d)),d(plot_idx),'UniformOutput',0)],'Interpreter','Latex')
 
 figure(3)
-plot(d,H,'ko'), title('Sum of Squared Error as a function of d, H(d)');
+plot(d,H,'ko'), title('Sum of Squared Error as a function of d, H(d)'), xlabel('d'), ylabel('H(d)');
 
--- project2/project2b.m
+++ project2/project2b.m
@@ -22,19 +22,24 @@ q = phi*a'; % q vector from completing the square
 q_delta = q.restricted_to(-T,tau); % Restrict (or zero pad) to the support [-d,d]. See FinSupFun.
 r = p \ q_delta; % Solving incorported in the FinSupFun class
 xhat0 = r.*y; % Construct estimate
+del = r*a; % Estimate of delta function
 
 % Calculate H
 H = phi.f(-phi.l+1) + q_delta.f*r.f'; % Theoretical H = f_0 + <q,P^(-1)q>
 
 figure(1)
+subplot(121)
 hb = plot(b.l:b.r, b.f,'g','LineWidth',3);
 hold on
 hf = plot(phi.l:phi.r, phi.f,'m','LineWidth',3);
 ha = plot(a.l:a.r, a.f,'b','LineWidth',3);
 hp = plot(p.l:p.r, p.f,'r','LineWidth',3);
+hr = plot(r.l:r.r, r.f,'c','LineWidth',3);
+hd = plot(del.l:del.r, del.f,'k','LineWidth',3);
 hold off
 grid on
-legend([hb hf ha hp], 'b', '\phi', 'a',  'p');
+legend([hb hf ha hp hr hd], {'b', '$\phi$', 'a',  'p', 'r', '$r * a$'}, 'Interpreter','Latex');
+title(sprintf('Covariance PSFs for Deconvolution [-T,\tau] =[-%d,%d]',T,tau))
 
 figure(2);
 subplot(311)
@@ -46,10 +51,11 @@ hold off
 grid on
 legend([hm hx hy], '\mu', 'x',  'y', 'Location','northwest');
 
-subplot(312)
+subplot(313)
 hold all 
 plot(x.l:x.r,x.f,'LineWidth',3);
 plot(xhat0.l:xhat0.r,xhat0.f,'LineWidth',3) 
+patch([xhat0.l:xhat0.r xhat0.r:-1:xhat0.l],[xhat0.f+sqrt(H) fliplr(xhat0.f-sqrt(H))], [0 1 0], 'facealpha', .3);
 hold off
 title(sprintf('r PSF support [-%d,%d]',T,tau))
 legend({'x',sprintf('$\\widehat{x}$, [-%d,%d]',T,tau)},'Interpreter','Latex','Location','northwest')
@@ -59,19 +65,20 @@ grid on
 [T,tau] = meshgrid(1:10,1:10);
 H = zeros(size(T)); % Initialize H
 xhat(length(T(:))) = FinSupFun(zeros(1,100)); % Initialize struct array of reconstructions
+r(length(T(:))) = FinSupFun(zeros(1,100)); % Initialize struct array of reconstructions
 for k = 1:length(T(:)) 
   [i,j] = ind2sub(size(T),k);
   q_delta = q.restricted_to(-T(i,j),tau(i,j)); % Restrict P and q to the support [-d,d]
-  r = p \ q_delta; % Solving incorported in the FinSupFun class
-  if length(r.f) < length(y.f) % Inner convolution is not symmetric. Need the smaller one to go first. 
-    xhat(k) = r.*y; % Construct estimate
+  r(k) = p \ q_delta; % Solving incorported in the FinSupFun class
+  if length(r(i).f) < length(y.f) % Inner convolution is not symmetric. Need the smaller one to go first. 
+    xhat(k) = r(k).*y; % Construct estimate
   else
-    xhat(k) = y.*r; % Construct estimate
+    xhat(k) = y.*r(k); % Construct estimate
   end
-  H(i,j) = phi.f(-phi.l+1) - q_delta.f*r.f'; % Theoretical H = f_0 + <q,P^(-1)q>
-end
+  H(k) = phi.f(-phi.l+1) - q_delta.f*r(k).f'; % Theoretical H = f_0 + <q,P^(-1)q>
+end 
 
-subplot(313)
+subplot(312)
 hold all 
 plot(x.l:x.r,x.f,'LineWidth',3);
 plot_idx = 1:ceil(length(xhat)/4):length(xhat); % Plot 4 reconstructions
@@ -83,15 +90,18 @@ title('Reconstructions for various d')
 legend(['x',arrayfun(@(idx)( sprintf('$\\widehat{x}$, [-%d,%d]',T(idx),tau(idx))),plot_idx,'UniformOutput',0)],'Interpreter','Latex','Location','northwest')
 grid on
 
+figure(1)
+subplot(122)
+title('PSFs for a*f')
+hold all
+for j = plot_idx
+  del = r(j) * a;
+  plot(del.l:del.r,del.f, 'linewidth',3); 
+end
+hold off
+grid on
+legend(['x',arrayfun(@(idx)( sprintf('$\\widehat{x}$, [-%d,%d]',T(idx),tau(idx))),plot_idx,'UniformOutput',0)],'Interpreter','Latex','Location','northwest')
+
 figure(3)
 surf(T,tau,log(H)), title('Natural Log of Sum of Squared Error as a function of d, log H(d)'), xlabel('T'), ylabel('\tau');
 
-%figure(4)
-%idx = find(T(:) == 7 & tau(:) == 5); 
-%hold all 
-%plot(x.l:x.r,x.f,'LineWidth',3);
-%plot(xhat(idx).l:xhat(idx).r,xhat(idx).f,'LineWidth',3) 
-%hold off
-%title(sprintf('r PSF support [-%d,%d]',T,tau))
-%legend({'x',sprintf('$\\widehat{x}$, [-%d,%d]',T,tau)},'Interpreter','Latex')
-%grid on
